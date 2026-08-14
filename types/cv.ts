2a2ca3bd0e0ce3bf5cffb2a4f7d3ae62bc17fae6
Amos N'cho <nchoamos216@gmail.com>
2026-08-14 23:50:01 +0000

    Ajout link a Project et types finaux

--- types/cv.ts
+++ types/cv.ts
@@ -34,6 +34,7 @@ export interface Project {
   title?: string;
   description?: string;
   url?: string;
+  link?: string;
   technologies?: string[] | string;
   startDate?: string;
   endDate?: string;
